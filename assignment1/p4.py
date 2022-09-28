5a588ecde64bae46b947b68e589aef19d1b8ced9
Luke Eltiste <luke343279@gmail.com>
2022-09-27 18:36:17 -0700

    part 4 and 5

--- assignment1/p4.py
+++ assignment1/p4.py
@@ -10,14 +10,13 @@ from utils import increase_order, normalize
 np.set_printoptions(precision=2, linewidth=127)
 
 # variables
-lam = 0.001  # Reduction tuning parameter
-alpha = 0.01  # tuning parameter
-iters = 2_000  # number of iterations
-order = 1  # order of data
+order = 2  # order of data
+lam = 10**-5  # Reduction tuning parameter
 percent = 0.5  # Amount of data used for training
 
 # Load the dataset
 df = pd.read_csv("Data1.csv")
+
 
 split_index = int(len(df) * percent)
 
@@ -31,6 +30,7 @@ print("Order set")
 
 
 print(data_X)
+print()
 
 # Split the data into training/testing sets
 data_X_train = data_X[:split_index]
@@ -41,7 +41,6 @@ data_X_test = data_X[split_index:]
 data_y_train = data_y[:split_index]
 data_y_test = data_y[split_index:]
 
-print(f"Least Squares Method")
 
 # Create linear regression object
 regr = linear_model.LinearRegression()
@@ -54,19 +53,21 @@ regr.fit(data_X_train, data_y_train)
 print(f"Training Took {time()-start_time:.2} seconds")
 print()
 
-
-print(f"Running Ridge Reduction On The Model")
 W = regr.coef_.copy()
 
 # Ridge reduction of model
+print(f"Running Ridge Reduction On The Model")
+
 for i in range(len(regr.coef_)):
     regr.coef_[i] = regr.coef_[i] + lam * regr.coef_[i] ** 2
+
 
 # Make predictions using the testing set
 data_y_pred_test = regr.predict(data_X_test)
 data_y_pred_train = regr.predict(data_X_train)
 print("Coefficients:")
 print(regr.coef_)
+
 # The mean squared error
 print(f"RMSE Train : {sqrt(mse(data_y_train, data_y_pred_train)).real:.2f}")
 # The mean squared error
@@ -79,8 +80,9 @@ print(f"R^2 Test: {r2_score(data_y_test, data_y_pred_test):.2f}")
 print()
 
 
-print(f"Running LASSO Reduction On The Model")
 # LASSO reduction of model
+print(f"Running LASSO Reduction On The Model")
+
 regr.coef_ = W.copy()
 for i in range(len(regr.coef_)):
     regr.coef_[i] = regr.coef_[i] + lam * abs(regr.coef_[i])
@@ -91,6 +93,7 @@ data_y_pred_test = regr.predict(data_X_test)
 data_y_pred_train = regr.predict(data_X_train)
 print("Coefficients:")
 print(regr.coef_)
+
 # The mean squared error
 print(f"RMSE Train : {sqrt(mse(data_y_train, data_y_pred_train)).real:.2f}")
 # The mean squared error
@@ -103,8 +106,9 @@ print(f"R^2 Test: {r2_score(data_y_test, data_y_pred_test):.2f}")
 print()
 
 
-print(f"Running Elastic Reduction On The Model")
 # Elastic reduction of model (LASSO has already been applied, so we will just reapply Ridge)
+print(f"Running Elastic Reduction On The Model")
+
 for i in range(len(regr.coef_)):
     regr.coef_[i] = regr.coef_[i] + lam * regr.coef_[i] ** 2
 
@@ -114,6 +118,7 @@ data_y_pred_test = regr.predict(data_X_test)
 data_y_pred_train = regr.predict(data_X_train)
 print("Coefficients:")
 print(regr.coef_)
+
 # The mean squared error
 print(f"RMSE Train : {sqrt(mse(data_y_train, data_y_pred_train)).real:.2f}")
 # The mean squared error
@@ -124,123 +129,3 @@ print(f"RMSE Test : {sqrt(mse(data_y_test, data_y_pred_test)).real:.2f}")
 # The mean squared error
 print(f"R^2 Test: {r2_score(data_y_test, data_y_pred_test):.2f}")
 print()
-print()
-
-
-print(f"Gradient Descent Method")
-print()
-
-# Gradient Descent
-n, k = data_X_train.shape
-w = np.ones(k)
-
-print(f"Running Ridge Reduction On The Model")
-
-# Ridge Reduction of model
-data_X_trans = data_X_train.T
-for iteration in range(iters):
-    pred = np.dot(data_X_train, w)
-    loss = pred - data_y_train
-
-    if iteration % 1000 == 0:
-        # Print out the cost and weights every 1000 iterations
-        cost = np.sum(loss**2) / (2 * n)
-        print(f"Iteration {iteration} | Cost {cost:.2f} | Weights: {w}")
-
-    gradient = np.dot(data_X_trans, loss) / n
-
-    w = w * (1 - 2 * lam * alpha) - (alpha * gradient)
-
-print(f"Training Took {time()-start_time:.2f} seconds")
-
-print(f"Final Weight {w}")
-
-# Save Weights
-Ridge_Gradient_Weights = w.copy()
-
-# Make predictions using the testing set
-data_y_pred_train = np.dot(data_X_train, w).T
-data_y_pred_test = np.dot(data_X_test, w).T
-
-print("Coefficients:")
-print(w)
-# The mean squared error
-print(f"RMSE Train : {sqrt(mse(data_y_train, data_y_pred_train)).real:.2f}")
-# The mean squared error
-print(f"R^2 Train: {r2_score(data_y_train, data_y_pred_train):.2f}")
-
-# The mean squared error
-print(f"RMSE Test : {sqrt(mse(data_y_test, data_y_pred_test)).real:.2f}")
-# The mean squared error
-print(f"R^2 Test: {r2_score(data_y_test, data_y_pred_test):.2f}")
-print()
-
-""" LASSO not working
-
-print(f"Running LASSO Reduction On The Model")
-
-# LASSO Reduction of model
-w = np.ones(k)
-
-
-for iteration in range(iters):
-    # pred = np.dot(data_X_train, w)
-    # loss = pred - data_y_train
-
-    # if iteration % 1000 == 0:
-    #     # Print out the cost and weights every 1000 iterations
-    #     cost = np.sum(loss**2) / (2 * n)
-    #     print(f"Iteration {iteration} | Cost {cost:.2f} | Weights: {w}")
-
-    # gradient = np.dot(data_X_trans, loss) / n
-
-    # w = w * (1 - 2*lam*alpha) - (alpha * gradient)
-    if iteration % 10 == 0:
-        # Print out the cost and weights every 1000 iterations
-        cost = np.sum(loss**2) / (2 * n)
-        print(f"Iteration {iteration} | Cost {cost:.2f} | Weights: {w}")
-
-    pred_rows = []
-    for i in range(n):
-        pred_rows.append(0)
-        for m in range(k):
-            pred_rows[i] += data_X_train[i][m] * w[m]
-
-    rho = np.zeros(k)
-    for j in range(k):
-        for i in range(n):
-            error = pred_rows[i] - data_X_train[i][j] * w[j]
-            rho[j] += data_X_train[i][j] * error
-    
-    for j in range(k):
-        if rho[j] < -lam/2:
-            w[j] = rho[j] + lam/2
-        elif rho[j] > lam/2:
-            w[j] = rho[j] - lam/2
-        else:
-            w[j] = 0
-
-
-print(f"Training Took {time()-start_time:.2f} seconds")
-
-print(f"Final Weight {w}")
-
-# Save Weights
-
-# Make predictions using the testing set
-data_y_pred_train = np.dot(data_X_train, w).T
-data_y_pred_test = np.dot(data_X_test, w).T
-
-print("Coefficients:")
-print(w)
-# The mean squared error
-print(f"RMSE Train : {sqrt(mse(data_y_train, data_y_pred_train)).real:.2f}")
-# The mean squared error
-print(f"R^2 Train: {r2_score(data_y_train, data_y_pred_train):.2f}")
-
-# The mean squared error
-print(f"RMSE Test : {sqrt(mse(data_y_test, data_y_pred_test)).real:.2f}")
-# The mean squared error
-print(f"R^2 Test: {r2_score(data_y_test, data_y_pred_test):.2f}")
-print()
-"""
---
+++ assignment1/p5.py
@@ -0,0 +1,188 @@
+from cmath import sqrt
+
+# from select import kevent
+from time import time
+import pandas as pd
+import numpy as np
+from sklearn import linear_model
+from sklearn.metrics import mean_squared_error as mse, r2_score
+
+from utils import increase_order, normalize
+
+np.set_printoptions(precision=2, linewidth=127)
+
+# variables
+order = 2  # order of data
+lam = 10**-5  # Reduction tuning parameter
+K_folds = 4  # Number of partitions the data will be divided into
+
+# Load the dataset
+df = pd.read_csv("Data1.csv")
+
+
+partition_size = len(df) / K_folds
+
+imported_data_y = df.drop(columns=["T", "P", "TC", "SV"]).to_numpy().T[0]
+imported_data_X = df.drop(columns="Idx").to_numpy()
+
+
+imported_data_X = increase_order(mat=imported_data_X, order=order)
+imported_data_X = normalize(imported_data_X)
+print("Order set")
+
+
+print(imported_data_X)
+print()
+
+# Split the data into equally sized sets
+data_X = []
+for k in range(K_folds):
+    data_X.append(imported_data_X[int(k * partition_size) : int((k + 1) * partition_size)])
+
+
+# Split the targets into equally sized sets
+data_y = []
+for k in range(K_folds):
+    data_y.append(imported_data_y[int(k * partition_size) : int((k + 1) * partition_size)])
+
+
+# Create linear regression object
+regr = linear_model.LinearRegression()
+
+# Train the model using the training sets
+print("Start Training")
+start_time = time()
+
+
+Ridge_models = []
+Lasso_models = []
+Elastic_models = []
+
+for k in range(K_folds):
+    regr.fit(data_X[k], data_y[k])
+    print(f"Training Took {time()-start_time:.2} seconds")
+    print()
+
+    W = regr.coef_.copy()
+
+    # Ridge reduction of model
+    print(f"Running Ridge Reduction On The Model")
+
+    for i in range(len(regr.coef_)):
+        regr.coef_[i] = regr.coef_[i] + lam * regr.coef_[i] ** 2
+
+    # Add model to list of Ridge models
+    Ridge_models.append(regr.coef_)
+
+    # Make predictions using the testing set
+    data_y_pred_test = regr.predict(data_X[k])
+    data_y_pred_train = regr.predict(data_X[k])
+    print("Coefficients:")
+    print(regr.coef_)
+
+    # The mean squared error
+    print(f"RMSE Train : {sqrt(mse(data_y[k], data_y_pred_train)).real:.2f}")
+    # The mean squared error
+    print(f"R^2 Train: {r2_score(data_y[k], data_y_pred_train):.2f}")
+
+    # The mean squared error
+    print(f"RMSE Test : {sqrt(mse(data_y[k], data_y_pred_test)).real:.2f}")
+    # The mean squared error
+    print(f"R^2 Test: {r2_score(data_y[k], data_y_pred_test):.2f}")
+    print()
+
+    # LASSO reduction of model
+    print(f"Running LASSO Reduction On The Model")
+
+    regr.coef_ = W.copy()
+    for i in range(len(regr.coef_)):
+        regr.coef_[i] = regr.coef_[i] + lam * abs(regr.coef_[i])
+
+    # Add model to list of LASSO models
+    Lasso_models.append(regr.coef_)
+
+    # Make predictions using the testing set
+    data_y_pred_test = regr.predict(data_X[k])
+    data_y_pred_train = regr.predict(data_X[k])
+    print("Coefficients:")
+    print(regr.coef_)
+
+    # The mean squared error
+    print(f"RMSE Train : {sqrt(mse(data_y[k], data_y_pred_train)).real:.2f}")
+    # The mean squared error
+    print(f"R^2 Train: {r2_score(data_y[k], data_y_pred_train):.2f}")
+
+    # The mean squared error
+    print(f"RMSE Test : {sqrt(mse(data_y[k], data_y_pred_test)).real:.2f}")
+    # The mean squared error
+    print(f"R^2 Test: {r2_score(data_y[k], data_y_pred_test):.2f}")
+    print()
+
+    # Elastic reduction of model (LASSO has already been applied, so we will just reapply Ridge)
+    print(f"Running Elastic Reduction On The Model")
+
+    for i in range(len(regr.coef_)):
+        regr.coef_[i] = regr.coef_[i] + lam * regr.coef_[i] ** 2
+
+    # Add model to list of Elastic models
+    Elastic_models.append(regr.coef_)
+
+    # Make predictions using the testing set
+    data_y_pred_test = regr.predict(data_X[k])
+    data_y_pred_train = regr.predict(data_X[k])
+    print("Coefficients:")
+    print(regr.coef_)
+
+    # The mean squared error
+    print(f"RMSE Train : {sqrt(mse(data_y[k], data_y_pred_train)).real:.2f}")
+    # The mean squared error
+    print(f"R^2 Train: {r2_score(data_y[k], data_y_pred_train):.2f}")
+
+    # The mean squared error
+    print(f"RMSE Test : {sqrt(mse(data_y[k], data_y_pred_test)).real:.2f}")
+    # The mean squared error
+    print(f"R^2 Test: {r2_score(data_y[k], data_y_pred_test):.2f}")
+    print()
+# End of for loop
+
+# Get the mean weights for Ridge model
+Sums = []
+for j in range(len(Ridge_models[0])):
+    Sums.append(0)
+    for i in range(len(Ridge_models)):
+        Sums[j] += Ridge_models[i][j]
+for sum in Sums:
+    sum /= len(Ridge_models)
+
+# Mean Ridge model
+print(f"Mean Weights Of Ridge Models:")
+print(Sums)
+print()
+
+# Get the mean weights for LASSO model
+Sums = []
+for j in range(len(Lasso_models[0])):
+    Sums.append(0)
+    for i in range(len(Lasso_models)):
+        Sums[j] += Lasso_models[i][j]
+for sum in Sums:
+    sum /= len(Lasso_models)
+
+# Mean LASSO model
+print(f"Mean Weights Of LASSO Models:")
+print(Sums)
+print()
+
+# Get the mean weights for Elastic model
+Sums = []
+for j in range(len(Elastic_models[0])):
+    Sums.append(0)
+    for i in range(len(Elastic_models)):
+        Sums[j] += Elastic_models[i][j]
+for sum in Sums:
+    sum /= len(Elastic_models)
+
+# Mean Ridge model
+print(f"Mean Weights Of Elastic Models:")
+print(Sums)
+print()
